d0e0364503560362a815aded7b0c7b2b8ed91305
LokeshRaju <loki65628@gmail.com>
2026-01-29 22:31:26 +0530

    Time: 15 ms (72.37%), Space: 34.7 MB (84.36%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -6,14 +6,13 @@ public:
 
         int m = mat.size(), n = mat[0].size();
         queue<pair<int, int>> queue;
-        int MAX_VALUE = m * n;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
                     queue.push({i, j});
                 } else {
-                    mat[i][j] = MAX_VALUE;
+                    mat[i][j] = -1;
                 }
             }
         }
@@ -24,7 +23,7 @@ public:
             auto [row, col] = queue.front(); queue.pop();
             for (auto [dr, dc] : directions) {
                 int r = row + dr, c = col + dc;
-                if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] > mat[row][col] + 1) {
+                if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] == -1) {
                     queue.push({r, c});
                     mat[r][c] = mat[row][col] + 1;
                 }
