6913c30fee818417a3e3d108d84f9138301e264c
LokeshRaju <loki65628@gmail.com>
2026-01-29 21:35:20 +0530

    Time: 0 ms (100%), Space: 16.8 MB (57.93%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -69,49 +69,60 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<int,int>> q;
+        queue<pair<int,int>>q;
         int fresh = 0;
-        int time = 0;
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 2) {
-                    q.push({i, j});
-                } else if(grid[i][j] == 1) {
+                if(grid[i][j] == 2)
+                {
+                    q.push({i,j});
+                }
+                else if(grid[i][j] == 1)
+                {
                     fresh++;
                 }
             }
         }
 
-        if(fresh == 0) return 0;
+        if(fresh == 0)
+        {
+            return 0;
+        }
 
-        int dx[] = {1, -1, 0, 0};
-        int dy[] = {0, 0, 1, -1};
-
-        while(!q.empty()) {
-            int levelsize = q.size();
-            while(levelsize--) {
-                auto temp = q.front();
+        int time = 0;
+        vector<pair<int,int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.empty())
+        {
+            int size = q.size();
+            while(size--)
+            {
+                auto[row,col] = q.front();
                 q.pop();
 
-                for(int k = 0; k < 4; k++) {
-                    int tempx = temp.first + dx[k];
-                    int tempy = temp.second + dy[k];
+                for(auto [x,y] : directions)
+                {
+                    int r = row + x;
+                    int c = col + y;
 
-                    if(tempx >= 0 && tempy >= 0 &&
-                       tempx < n && tempy < m &&
-                       grid[tempx][tempy] == 1) {
-
-                        grid[tempx][tempy] = 2;  // mark rotten
-                        q.push({tempx, tempy});
-                        fresh--;
+                    if(r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == 1)
+                    {
+                        q.push({r,c});
+                        grid[r][c] = 2;
+                        fresh--;//after rotting the orange the fresh fruit turns into rotten then we dec fresh orange size
                     }
                 }
             }
-            time++;
+            time++;//after rotting all adjacent fresh oranges
         }
 
-        if(fresh == 0) return time - 1;
+        if(fresh == 0)
+        {
+            return time - 1;
+        }
+
         return -1;
     }
 };
