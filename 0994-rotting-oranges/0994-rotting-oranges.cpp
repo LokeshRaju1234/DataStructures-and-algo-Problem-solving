cfc0a18e1e0309475de404bedba6840255f37681
LokeshRaju <loki65628@gmail.com>
2026-01-29 10:17:58 +0530

    Time: 0 ms (100%), Space: 16.6 MB (87.27%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,117 @@
+// class Solution {
+// public:
+//     int orangesRotting(vector<vector<int>>& grid) {
+//         int n = grid.size();
+//         int m = grid[0].size();
+
+//         queue<pair<int,int>>q;
+//         int fresh = 0;
+//         int time = 0;
+//         for(int i = 0;i < n;i++)
+//         {
+//             for(int j = 0;j < m;j++)
+//             {
+//                 if(grid[i][j] == 2)
+//                 {
+//                     q.push({i,j});
+//                 }
+//                 else if(grid[i][j] == 1)
+//                 {
+//                     fresh++;
+//                 }
+//             }
+//         }
+
+//         if(fresh == 0)
+//         {
+//             return 0;//if no fresh oranges are there
+//         }
+//         int dx[] = {1,-1,0,0};
+//         int dy[] = {0,0,1,-1};
+
+//         while(q.size() > 0)
+//         {
+//             int levelsize = q.size();
+//             while(levelsize--)
+//             {
+//                 pair<int,int> temp = q.front();
+//                 q.pop();
+
+//                 for(int k = 0;k < 4;k++)
+//                 {
+//                     int tempx = temp.first + dx[k];
+//                     int tempy = temp.first + dy[k];
+//                     if(tempx >= 0 && tempy >= 0 && tempx < n && tempy < m && grid[tempx][tempy] == 1)
+//                     {
+//                         grid[tempx][tempy] = 2;
+//                         q.push({tempx,tempy});
+//                         fresh--;
+//                     }
+//                 }
+//             }
+
+//             time++;
+//         }
+
+//         if(fresh == 0)
+//         {
+//             return time - 1;
+//         }
+
+//         return -1;
+        
+//     }
+// };
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int,int>> q;
+        int fresh = 0;
+        int time = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 2) {
+                    q.push({i, j});
+                } else if(grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        if(fresh == 0) return 0;
+
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
+
+        while(!q.empty()) {
+            int levelsize = q.size();
+            while(levelsize--) {
+                auto temp = q.front();
+                q.pop();
+
+                for(int k = 0; k < 4; k++) {
+                    int tempx = temp.first + dx[k];
+                    int tempy = temp.second + dy[k];
+
+                    if(tempx >= 0 && tempy >= 0 &&
+                       tempx < n && tempy < m &&
+                       grid[tempx][tempy] == 1) {
+
+                        grid[tempx][tempy] = 2;  // mark rotten
+                        q.push({tempx, tempy});
+                        fresh--;
+                    }
+                }
+            }
+            time++;
+        }
+
+        if(fresh == 0) return time - 1;
+        return -1;
+    }
+};
