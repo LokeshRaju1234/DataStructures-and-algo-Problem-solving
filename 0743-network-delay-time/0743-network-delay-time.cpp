f2ea0b65625a820cf9cc577f735eea77b2e79a39
LokeshRaju <loki65628@gmail.com>
2026-02-01 11:41:02 +0530

    Time: 84 ms (54.96%), Space: 44.1 MB (91.24%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -2,50 +2,51 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
 
-        vector<vector<pair<int,int>>> adj(n);
+        vector<vector<pair<int,int>>>adj(n);
 
-        // build graph (convert to 0-based index)
-        for (auto &e : times) {
-            int u = e[0] - 1;
-            int v = e[1] - 1;
-            int w = e[2];
-            adj[u].push_back({v, w});
+        for(int i = 0;i < times.size();i++)
+        {
+            int u = times[i][0] - 1;
+            int v = times[i][1] - 1;
+            int w = times[i][2];
+            adj[u].push_back({v,w});
         }
 
-        vector<int> dist(n, 1e9);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+
+        vector<int>dist(n,1e9);
         dist[k - 1] = 0;
+        pq.push({0,k - 1});
 
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>
-        > pq;
-
-        pq.push({0, k - 1});
-
-        while (!pq.empty()) {
-            auto [d, node] = pq.top();
+        while(!pq.empty())
+        {
+            auto [d,n] = pq.top();
             pq.pop();
 
-            if (d > dist[node]) continue;
+            if(d > dist[n]) continue;
+            for(auto neigh : adj[n])
+            {
+                int node = neigh.first;
+                int wt = neigh.second;
 
-            for (auto &neigh : adj[node]) {
-                int next = neigh.first;
-                int wt   = neigh.second;
-
-                if (dist[next] > d + wt) {
-                    dist[next] = d + wt;
-                    pq.push({dist[next], next});
+                if(dist[node] > d + wt)
+                {
+                    dist[node] = d + wt;
+                    pq.push({dist[node],node});
                 }
             }
         }
+        
+int ans = 0;
+        for(int i = 0;i < n;i++)
+        {
+            if(dist[i] == 1e9)
+            {
+                return -1;
+            }
 
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (dist[i] == 1e9) return -1;
-            ans = max(ans, dist[i]);
+            ans = max(ans,dist[i]);
         }
-
         return ans;
     }
 };
